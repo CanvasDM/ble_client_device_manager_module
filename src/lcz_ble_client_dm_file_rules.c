baa60218f2558a8aa8b4138b3cee06784936a99d
Randy Scott <randy.scott@lairdconnect.com>
2022-09-14 13:15:40 -0500

    Catching client up with gateway

---
+++ src/lcz_ble_client_dm_file_rules.c
@@ -0,0 +1,125 @@
+/**
+ * @file lcz_ble_client_dm_file_rules.c
+ * @brief Holds rules for file access for the DM gateway
+ *
+ * Copyright (c) 2022 Laird Connectivity LLC
+ *
+ * SPDX-License-Identifier: LicenseRef-LairdConnectivity-Clause
+ */
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(lcz_ble_client_dm_file_rules, CONFIG_LCZ_BLE_CLIENT_DM_LOG_LEVEL);
+
+/**************************************************************************************************/
+/* Includes                                                                                       */
+/**************************************************************************************************/
+#include <zephyr.h>
+#include <init.h>
+
+#include "file_system_utilities.h"
+#include "encrypted_file_storage.h"
+
+#if defined(CONFIG_ATTR)
+#include "attr.h"
+#endif
+#if defined(CONFIG_LCZ_FS_MGMT_FILE_ACCESS_HOOK)
+#include <lcz_fs_mgmt/lcz_fs_mgmt.h>
+#endif
+#if defined(CONFIG_LCZ_LWM2M_FS_MANAGEMENT)
+#include "lcz_lwm2m_obj_fs_mgmt.h"
+#endif
+
+/**************************************************************************************************/
+/* Local Constant, Macro and Type Definitions                                                     */
+/**************************************************************************************************/
+#ifdef ATTR_ID_factory_load_path
+/* How long after the last write will writes to the factory load path still succeed */
+#define FACTORY_WRITE_DURATION K_SECONDS(1)
+#endif
+
+/**************************************************************************************************/
+/* Local Function Prototypes                                                                      */
+/**************************************************************************************************/
+static int lcz_ble_client_dm_file_rules_init(const struct device *device);
+static bool client_dm_file_test(const char *path, bool write);
+static void factory_write_work_handler(struct k_work *work);
+
+/**************************************************************************************************/
+/* Local Data Definitions                                                                         */
+/**************************************************************************************************/
+#ifdef ATTR_ID_factory_load_path
+static K_WORK_DELAYABLE_DEFINE(factory_write_work, factory_write_work_handler);
+#endif
+
+/**************************************************************************************************/
+/* Local Function Definitions                                                                     */
+/**************************************************************************************************/
+static bool client_dm_file_test(const char *path, bool write)
+{
+	char simple_path[FSU_MAX_ABS_PATH_SIZE + 1];
+	char *load_path;
+
+	/* Simplify the path */
+	if (fsu_simplify_path(path, simple_path) < 0) {
+		/* If the simplification failed, deny access */
+		return false;
+	}
+
+	/* Allow any reads and writes to non-encrypted paths */
+	if (efs_is_encrypted_path(simple_path) == false) {
+		return true;
+	}
+
+	/* Reads of encrypted files are not allowed */
+	if (write == false) {
+		return false;
+	}
+
+	/* Write of the attribute load file is allowed */
+#ifdef ATTR_ID_load_path
+	load_path = (char *)attr_get_quasi_static(ATTR_ID_load_path);
+	if (strcmp(load_path, simple_path) == 0) {
+		return true;
+	}
+#endif
+
+	/* If the factory load file does not exist, it can be written */
+#ifdef ATTR_ID_factory_load_path
+	load_path = (char *)attr_get_quasi_static(ATTR_ID_factory_load_path);
+	if (strcmp(load_path, simple_path) == 0) {
+		if (k_work_delayable_is_pending(&factory_write_work)) {
+			/* Write of the factory file is pending */
+			k_work_reschedule(&factory_write_work, FACTORY_WRITE_DURATION);
+			return true;
+		} else if (efs_get_file_size(simple_path) < 0) {
+			/* New write of factory file is allowed */
+			k_work_reschedule(&factory_write_work, FACTORY_WRITE_DURATION);
+			return true;
+		}
+	}
+#endif
+
+	/* Reject anything else by default */
+	return false;
+}
+
+static void factory_write_work_handler(struct k_work *work)
+{
+	/* Nothing to do */
+}
+
+/**************************************************************************************************/
+/* SYS INIT                                                                                       */
+/**************************************************************************************************/
+SYS_INIT(lcz_ble_client_dm_file_rules_init, APPLICATION, CONFIG_LCZ_CLIENT_DM_FILE_RULES_INIT_PRIORITY);
+static int lcz_ble_client_dm_file_rules_init(const struct device *device)
+{
+	/* Register our rules function with SMP and LwM2M */
+#if defined(CONFIG_LCZ_FS_MGMT_FILE_ACCESS_HOOK)
+	lcz_fs_mgmt_register_evt_cb(client_dm_file_test);
+#endif
+#if defined(CONFIG_LCZ_LWM2M_FS_MANAGEMENT)
+	lcz_lwm2m_obj_fs_mgmt_register_cb(client_dm_file_test);
+#endif
+	return 0;
+}
